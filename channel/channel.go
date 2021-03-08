c8d92fd31079ae48a45d3acb22cbc06973a566d5
dyr <dyr@cndatacom.com>
2021-03-08 15:52:40 +0800

    update

--- channel/channel.go
+++ channel/channel.go
@@ -12,35 +12,43 @@ func main() {
 	ch := make(chan int)
 
 	go func() {
-		fmt.Println("go 写入管道 1")
+		fmt.Println("go 写入管道： 1")
 		ch <- 1 //将1写入管道
-		fmt.Println("go 写入管道 2", time.Now())
+		fmt.Println("go 写入管道： 2", time.Now())
 		ch <- 2 //将2写入管道，管道第二个值写入要等待第一个值读出
 		fmt.Println("go 管道写入结束", time.Now())
 	}()
 
 	fmt.Println("channel begin")
-	num := <-ch //如果未读取到数据会阻塞
 	fmt.Println("channel 读取管道第一个值")
-	fmt.Println(num)
+	num := <-ch //如果未读取到数据会阻塞
+	fmt.Println("管道值输出打印：", num)
 	// time.Sleep(10 * time.Second)
-	fmt.Println("channel 读取管道第二个值")
-	fmt.Println("sleep 5s")
+
+	fmt.Println("管道第一个值读取完成后，sleep 5s 后，再次读取管道值，为演示阻塞")
 	time.Sleep(5 * time.Second)
+
+	fmt.Println("channel 读取管道第二个值")
 	num = <-ch
-	fmt.Println(num)
+	fmt.Println("管道值输出打印：", num)
 	time.Sleep(1 * time.Second)
+
+
+	fmt.Println("----------------------------------------------------------------------")
 
 	//遍历，需要同时关闭管道，否则无法遍历，因为range长度无法控制
 	go func() {
 		ch <- 1
+		fmt.Println("写入管道：1",time.Now())
 		ch <- 2
+		fmt.Println("写入管道：2",time.Now())
 		ch <- 3
+		fmt.Println("写入管道：3",time.Now())
 		close(ch)
 	}()
 
 	for num := range ch {
-		fmt.Println(num)
+		fmt.Println(num,time.Now())
 	}
 
 	// 关闭管道，管道关闭不能写，但是可以读
